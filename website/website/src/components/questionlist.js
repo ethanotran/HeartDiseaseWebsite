894ecb7afbc2b712bd5ec45fc26a089c02f95772
jtorruellas22 <116301075+jtorruellas22@users.noreply.github.com>
2024-10-23 15:02:10 -0400

    Update Quiz Functionality
    - Quiz component updated to take in text input instead of multiple choice answers
    - CSS file was updated to fix button width printing incorrectly
    - Question list updated with temporary questions that better direct users towards their answers
    - Fixed issue where quiz would skip last question
    - Added a function to validate user responses as well as prompt for invalid answers

--- website/website/src/components/questionlist.js
+++ website/website/src/components/questionlist.js
@@ -1,41 +1,64 @@
 export const data = [
   {
-    question: "Insert question 1",
-    option1: "First Answer Choice",
-    option2: "Second Answer Choice",
-    option3: "Third Answer Choice",
-    option4: "Fourth Answer Choice",
+    question: "How old are you?",
+    dataType: "number",
+    upper: 150,
+    lower: 0
   },
 
   {
-    question: "Insert question 2",
-    option1: "First Answer Choice",
-    option2: "Second Answer Choice",
-    option3: "Third Answer Choice",
-    option4: "Fourth Answer Choice",
+    question: "What gender are you?",
+    dataType: "text",
+    upper: 1,
+    lower: 0
   },
 
   {
-    question: "Insert question 3",
-    option1: "First Answer Choice",
-    option2: "Second Answer Choice",
-    option3: "Third Answer Choice",
-    option4: "Fourth Answer Choice",
+    question: "What is your impulse level?",
+    dataType: "number",
+    upper: 200,
+    lower: 0
   },
 
   {
-    question: "Insert question 4",
-    option1: "First Answer Choice",
-    option2: "Second Answer Choice",
-    option3: "Third Answer Choice",
-    option4: "Fourth Answer Choice",
+    question: "What is your systolic blood pressure? (Top number)",
+    dataType: "number",
+    upper: 200,
+    lower: 0
   },
 
   {
-    question: "Insert question 5",
-    option1: "First Answer Choice",
-    option2: "Second Answer Choice",
-    option3: "Third Answer Choice",
-    option4: "Fourth Answer Choice",
+    question: "What is your diastolic blood pressure? (Bottom number)",
+    dataType: "number",
+    upper: 200,
+    lower: 0
+  },
+
+  {
+    question: "What are is your glucose level?",
+    dataType: "number",
+    upper: 500,
+    lower: 0
+  },
+
+  {
+    question: "What is your kcm level?",
+    dataType: "number",
+    upper: 100,
+    lower: 0
+  },
+
+  {
+    question: "What is your troponin level?",
+    dataType: "number",
+    upper: 8,
+    lower: 0
+  },
+
+  {
+    question: "What is class are you?",
+    dataType: "text",
+    upper: 1,
+    lower: 0
   },
 ]
--- website/website/src/components/quizcomponent.js
+++ website/website/src/components/quizcomponent.js
@@ -5,48 +5,67 @@ import { data } from "./questionlist";
 // Code inspired by tutorial from GreatStack Youtube Channel, https://www.youtube.com/watch?v=VMZ7lcSdVnY
 const QuizComponent = () => {
 
-  let [index, setIndex] = useState(0);
-  let [question, setQuestion] = useState(data[index]);
-  let [lock, setLock] = useState(false);
-  let [finish, setFinish] = useState(false);
+  let [index, setIndex] = useState(0)
+  let [lock, setLock] = useState(false)
+  let [question, setQuestion] = useState(data[index])
+  let [finish, setFinish] = useState(false)
+  let temp
+  let userResponse = new Array(data.length)
 
-  let Option1 = useRef(null);
-  let Option2 = useRef(null);
-  let Option3 = useRef(null);
-  let Option4 = useRef(null);
-  
+  const validateResponse = () => {
+    temp = document.getElementById("answer").value
+    if (temp === "") {
+      return
+    }
+    if (typeof temp === "string") {
+      if (temp === "male" || temp === "positive") {
+        temp = 1
+      } else if (temp === "female" || temp === "negative") {
+        temp = 0
+      }
+    }
 
-  let optionArray = [Option1, Option2, Option3, Option4];
+    if (temp <= data[index].upper && temp >= data[index].lower) {
+      setAnswer(temp)
+      nextQuestion()
+    } else {
+      alert("Invalid response: Answer cannot be recognized by model")
+    }
+  }
 
   const setAnswer = (e) => {
-      setLock(true);
+    // Save user response
+    userResponse[index] = e
+    console.log(userResponse[index])
+    setLock(true)
   }
-  const next = () => {
+
+  const nextQuestion = () =>{
+
+    // Check for Question being answered
     if (lock === true) {
-      setIndex(++index);
-      setQuestion(data[index]);
-      setLock(false);
+      setIndex(++index)
+      setQuestion(data[index])
+      setLock(false)
     }
 
-    if (index === data.length-1) {
-      setFinish(true);
+    // Check if quiz is finished
+    if (index > data.length-1) {
+      setFinish(true)
+      setIndex(0)
+      // Here we would return the userResponse to our backend
     }
   }
-
+  
   return (
     <div className='container'>
-      <h1>Quiz Sector</h1>
-      <hr/>
-      {finish?<></>:<> <h2>{index+1}. {question.question}</h2>
-      <ul>
-        <li onClick={(e)=>{setAnswer(e, 1)}}>{question.option1}</li>
-        <li onClick={(e)=>{setAnswer(e, 2)}}>{question.option2}</li>
-        <li onClick={(e)=>{setAnswer(e, 3)}}>{question.option3}</li>
-        <li onClick={(e)=>{setAnswer(e, 4)}}>{question.option4}</li>
-      </ul>
-      <button onClick={next}>Next</button>
-      <div className="index"> Question {index+1} of {data.length} </div> </>}
-    </div>
+    <h1>Quiz Sector</h1>
+    <hr/>
+    {finish?<></>:<> <h2>{index+1}. {question.question}</h2>
+    <input type={data[index].dataType} id="answer"></input>
+    <button onClick={validateResponse}>Next</button>
+    <div className="index"> Question {index+1} of {data.length} </div> </>}
+  </div>
   )
 }
 
