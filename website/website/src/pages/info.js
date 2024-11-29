d431150ffd21513575aa7c4e4c1e3bc37023fc5c
ethanotran <ethanotran@gmail.com>
2024-11-28 22:49:49 -0800

    made grammattical changes to info and results pages

--- website/website/src/pages/info.js
+++ website/website/src/pages/info.js
@@ -32,7 +32,9 @@ export default function Info() {
 
           <h2 className="titles">How to Prevent Heart Disease? </h2>
 
-        <p>Having a healthy lifestyle is a good way of preventing any kind of heart disease from developing. Good diet, exercise and good sleep are a good start point for this.
+        <p>Having a healthy lifestyle is a good way of preventing any kind of heart disease from developing. Good diet, exercise and good sleep, in general are a good start in preventing 
+          conditions for heart disease. However, it is worth noting there are genetic factors in heart disease as well, such as a naturally high blood pressure or cholesterol. In cases like these,
+          it is even more crucial to be maintain a healthy lifestyle.
         </p>
      
       
@@ -40,12 +42,12 @@ export default function Info() {
           <h2 className="titles">Healthy Diet</h2>
  
         <p>Having a good balance between your meals contributes to preventing a healthy lifestyle and keeping your heart healthy. </p>
-        <p>high saturated and trans fats are a contributor for the development of heart conditions, it is good to reduce their consumption to as little as possible. 
-          This does not mean that all fats are bad, some dairy based foods and non-tropical fats like olive oil, avocados or oily fish in moderation do not increase the risk of heart diseases. 
-          Even low consumption of some dairy products like yogurt and cheese are beneficial for gut health. In the case of proteins, red meats are related to an increase of risk of cardiovascular disease so it is recommended to reduce its intake or even replace it with other proteins like poultry which in a moderate intake does not increase the risk for heart disease and  even plant based protein like legumes could be a good replacement. 
-          Also, keeping a constant consumption of fruits and vegetables is good for a balanced diet and its relation with low risk for heart diseases. 
+        <p>High saturated and trans fats are a major contributor in the development of heart disease. Therefor, it is good to reduce consumption of these foods. 
+          This does not mean that all fats are bad; some dairy based foods and non-tropical fats like olive oil, avocados or oily fish in moderation do not increase the risk of heart diseases. 
+          Also, some consumption of aged or fermented dairy products like yogurt and cheese are beneficial for gut health. In the case of proteins, red meats have been associted with an increased risk of cardiovascular disease. Poultry, in contrast does not increase the risk for heart disease, especially in moderate consumption. 
+          Finally, keeping a constant consumption of fruits and vegetables is good for a balanced diet and its relation with low risk for heart diseases. 
         </p>
-        <p>Consumption of salt and other high in sodium foods is related with increase in blood pressure, keeping this in a low intake is beneficial for health. 
+        <p>Consumption of salt and other high in sodium foods has been associated with increases in blood pressure. Therefore, reducing salt consumption can be beneficial for health. 
           In the case of sugar, keeping its consumption low will help prevent or control diabetes which is related to heart diseases. 
           This also includes the consumption of soft drinks which  intake should also be kept low. Drinks such as tea and coffee can be consumed regularly if sugar is not on them. Of course keeping a constant consumption of water is important. 
           Alcoholic drinks should be kept in moderation due to its relation with increase of blood pressure.
@@ -57,18 +59,18 @@ export default function Info() {
 
           <h2 className="titles" >Healthy Lifestyle</h2>
 
-        <p>Regular exercise is recommended to keep healthy. It is recommended to do at least 2:30 hours of constant exercise every week or 30 min per day. 
-          This will help prevent the development of any heart condition by keeping your weight, sugar in blood and blood pressure in check. 
-          Keeping your weight in check is important because being overweight puts stress on your heart and blood vessels which can develop heart diseases.
+        <p>Regular exercise is recommended to keep healthy. It is recommended to do at least 2:30 hours of constant exercise every week, or 30 min per day. 
+          This will help prevent the development of any heart condition by keeping your weight, blood sugar levels and blood pressure in check. 
+          Keeping your weight in check is important because being overweight leads to stress on your heart and blood vessels, which can develop conditions for heart disease.
         </p>
         <p>Avoid the consumption of cigarettes because these ones are related with the development of heart diseases.
-           Also keeping a good sleep schedule is recommended not just to prevent heart conditions but other illnesses.
+           Also, keeping a good sleep schedule is recommended not just to prevent heart conditions, but other illnesses.
             Between 7 to 9 hours is recommended for adults to keep a healthy sleeping schedule. If possible try to reduce mental stress. 
             Any mental practice is recommended to doing so. 
         </p>
         <p>Remember to always check your health with a specialized health team.
            It is important to be aware of your health and check on your medication even more if your conditions have a relation with heart disease. For example,
-            diabetes is a factor that increases the risk of heart disease so it is important to keep your diabetes medication in check and doing regular visits to your primary doctor. 
+            diabetes is a factor that increases the risk of heart disease so it is important to keep your diabetes medication in check and have regular check-ups with a physician. 
         </p>
         </div>
         </div>
