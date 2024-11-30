16fe57a17a29f0de50c1d92499138be98302b58a
ethanotran <ethanotran@gmail.com>
2024-11-29 23:56:50 -0800

    added changes to results page, model page and about us page

--- website/website/src/pages/about.js
+++ website/website/src/pages/about.js
@@ -1,12 +1,24 @@
 import NavBar from "../components/navbar";
+import React from "react";
 
 
 export default function About() {
   return(
     <body className="page1">
       <NavBar/>
-      <h1 className="header-box-info">About us Page</h1>
-      <p className="information">Team Description to Follow</p>
+      <h1 className="header-box-info">About Us</h1>
+      <p className="information">Alex Tran is a computer science student at the University of Florida, and has worked part time as a web developer at a small company focused on marine life conservation. 
+        His personal interests include reading, playing tennis, and operating systems, especially Linux.</p>
+        <p> Nicholas Tran is a fourth-year computer science student at the University Florida, and has also worked part time as a project manager and web development for Ocean Observation Group. 
+          After graduating this winter, he hopes to get a new job in entry-level software engineering or development. His interests include reading, swimming, and boxing. 
+        </p>
+        <p>Bernardo Rodriguez is a post-baccalaureate student with an initial degree in economics, going for a second bachelors in computer science. After graduating this Winter, 
+          he will begin a job as a software developer with Capital One in Virginia. His interests include reading, listening to music, playing video games, and weightlifting. 
+        </p>
+        <p>Jan Torruellas is a computer science student at the University of Florida. After graduation, he hopes to begin a new chapter working as an entry level software engineer/developer, 
+          and eventually begin moving his way up in the hierarchy with an end goal of becoming a project manager in the future. His interests include gaming, listening to music, swimming, and 
+          youth mentorship at his local church.
+        </p>
     </body>
   )
 }
